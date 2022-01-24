bf2dd4e29f0c65547035570b99a8ef66d3bbc9e0
CodeCraftPlugin <86466022+CodeCraftPlugin@users.noreply.github.com>
2022-01-24 14:26:45 +0530

    dontknow

--- src/main/java/net/codecraft/tutorialmod/item/ModItem.java
+++ src/main/java/net/codecraft/tutorialmod/item/ModItem.java
@@ -1,6 +1,7 @@
 package net.codecraft.tutorialmod.item;
 
 import net.codecraft.tutorialmod.TutorialMod;
+import net.codecraft.tutorialmod.item.advance.FireStone;
 import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -12,6 +13,12 @@ public class ModItem {
 
     public static final RegistryObject<Item> STEEL_INGOT = ITEMS.register("steel_ingot",()->new Item(
             new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP).maxStackSize(16)));
+    public static final RegistryObject<Item> FIRE_STONE = ITEMS.register("firestone",()->new FireStone(
+            new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP).maxDamage(12)));
+
+
+
+
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
